cfb142eb46051d3e2a2d2e94eb564282bc117712
borsuczyna <libxxerty@gmail.com>
2023-04-23 10:34:25 +0200

    animation interfaces

--- src/animations/main.ts
+++ src/animations/main.ts
@@ -1,3 +1,35 @@
 import animations from './animations.json';
 
-console.log(animations)
+interface Cycle {
+    frames: number[];
+    repeat: number;
+};
+
+interface Point {
+    x: number;
+    y: number;
+};
+
+interface Animation {
+    name: string;
+    fps: number;
+
+    offset: {
+        startX: number;
+        startY: number;
+        endX: number;
+        endY: number;
+        startTop: number;
+        endTop: string;
+    };
+
+    popup: {
+        x: number;
+        y: number;
+        direction: number;
+    };
+
+    size: number;
+    cycles: Cycle[];
+    points: Point[];
+};
