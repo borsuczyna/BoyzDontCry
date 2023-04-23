d94fdc30ac2e1bcdc312a605c51ddd5ee4c0fe2f
borsuczyna <libxxerty@gmail.com>
2023-04-23 10:47:28 +0200

    refactor

--- src/animations/main.ts
+++ src/animations/main.ts
@@ -1,35 +1,3 @@
-import animations from './animations.json';
+import animationsData from './animations.json';
+const animations: Animation[] = animationsData as unknown as Animation[];
 
-interface Cycle {
-    frames: number[];
-    repeat: number;
-};
-
-interface Point {
-    x: number;
-    y: number;
-};
-
-interface Animation {
-    name: string;
-    fps: number;
-
-    offset: {
-        startX: number;
-        startY: number;
-        endX: number;
-        endY: number;
-        startTop: number;
-        endTop: string;
-    };
-
-    popup: {
-        x: number;
-        y: number;
-        direction: number;
-    };
-
-    size: number;
-    cycles: Cycle[];
-    points: Point[];
-};
---
+++ src/animations/types.d.ts
@@ -0,0 +1,33 @@
+export interface Cycle {
+    frames: number[];
+    repeat: number;
+};
+
+export interface Point {
+    x: number;
+    y: number;
+};
+
+export interface Animation {
+    name: string;
+    fps: number;
+
+    offset: {
+        startX: number;
+        startY: number;
+        endX: number;
+        endY: number;
+        startTop: number;
+        endTop: string;
+    };
+
+    popup: {
+        x: number;
+        y: number;
+        direction: number;
+    };
+
+    size: number;
+    cycles: Cycle[];
+    points: Point[];
+};
